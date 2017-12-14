a93bfed2534b028a969de6bcf0bec372bb588ba2
Lorfe <lorfe-blade@hotmail.com>
2017-12-14 17:57:11 -0400

    Se termino de actualizar el model Registro

--- ContenedorPrincipal/Model/Registro.cs
+++ ContenedorPrincipal/Model/Registro.cs
@@ -9,7 +9,7 @@ namespace ContenedorPrincipal
 {
     class Registro
     {
-       
+
         private string nombre;
         private string apellido;
         private string dni;
@@ -17,7 +17,7 @@ namespace ContenedorPrincipal
         private string telefono;
         private string email;
         private string direccion;
-        private DateTime fechaNacimiento;
+        private DateTime fechanacimiento;
         private string sexo;
 
         
@@ -26,49 +26,61 @@ namespace ContenedorPrincipal
                          string telefono, string direccion, string email
                         )
         {
-            setNombre(nombre);
-            setApellido(apellido);
-            setCedula(cedula);
-            setDni(dni);
-            setTelefono(telefono);
-            setEmail(email);
-            setDireccion(direccion);
-            setFechaNacimiento(fechaNacimiento);
-            setSexo(sexo);
+            //setNombre(nombre);
+            //setApellido(apellido);
+            //setCedula(cedula);
+            //setDni(dni);
+            //setTelefono(telefono);
+            //setEmail(email);
+            //setDireccion(direccion);
+            //setFechaNacimiento(fechaNacimiento);
+            //setSexo(sexo);
             
 
         }
         public Registro(string nombre, string apellido, string dni, string cedula)
         {
-            setNombre(nombre);
-            setApellido(apellido);
-            setDni(dni);
-            setCedula(cedula);
+            //setNombre(nombre);
+            //setApellido(apellido);
+            //setDni(dni);
+            //setCedula(cedula);
            
 
         }
 
         public Registro() { }
 
-            public void setNombre(string nombre) { this.nombre = nombre; }
-            public void setApellido(string apellido) { this.apellido = apellido; }
-            public void setDni(string dni){this.dni = dni;}
-            private void setTelefono(string telefono) { this.telefono = telefono; }
-            private void setEmail(string email){ this.email = email; }
-            private void setDireccion(string direccion){ this.direccion = direccion; }
-            private  void setFechaNacimiento(DateTime fechaNacimiento){ this.fechaNacimiento = fechaNacimiento; }
-            private void setSexo(string sexo) { this.sexo = sexo; }
-         public void setCedula(string cedula) { this.cedula = cedula; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string dni { get; set; }
+        public string cedula { get; set; }
+        public string telefono { get; set; }
+        public string email { get; set; }
+        public string direccion { get; set; }
+        public DateTime fechanacimiento { get; set; }
+        public string sexo { get; set; }
 
-            public string GetNombre(){ return nombre; }
-            public string GetApellido(){ return apellido;}
-            public string GetDni()  { return dni; }
-            public string GetTelefono(){ return telefono; }
-            public string GetEmail(){ return email;}
-            public string GetDireccion(){ return direccion; }
-            public string GetDateTime(){return fechaNacimiento.Date.ToShortDateString();}
-            public string GetSexo(){ return sexo; }
-            public string GetCedula() { return cedula; }
+
+
+         //   public void setNombre(string nombre) { this.nombre = nombre; }
+         //   public void setApellido(string apellido) { this.apellido = apellido; }
+         //   public void setDni(string dni){this.dni = dni;}
+         //   private void setTelefono(string telefono) { this.telefono = telefono; }
+         //   private void setEmail(string email){ this.email = email; }
+         //   private void setDireccion(string direccion){ this.direccion = direccion; }
+         //   private  void setFechaNacimiento(DateTime fechaNacimiento){ this.fechaNacimiento = fechaNacimiento; }
+         //   private void setSexo(string sexo) { this.sexo = sexo; }
+         //public void setCedula(string cedula) { this.cedula = cedula; }
+
+         //   public string GetNombre(){ return nombre; }
+         //   public string GetApellido(){ return apellido;}
+         //   public string GetDni()  { return dni; }
+         //   public string GetTelefono(){ return telefono; }
+         //   public string GetEmail(){ return email;}
+         //   public string GetDireccion(){ return direccion; }
+         //   public string GetDateTime(){return fechaNacimiento.Date.ToShortDateString();}
+         //   public string GetSexo(){ return sexo; }
+         //   public string GetCedula() { return cedula; }
 
 
 
